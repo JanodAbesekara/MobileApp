8513a8969f999e651aa0612f049e53ea4331a98e
Samitha Wijenayake <samithamahesh@gmail.com>
2024-04-02 08:33:27 +0530

    Create Contact Us page

---
+++ dream_learn_app/dream_learn_app/lib/screens/contact_us.dart
@@ -0,0 +1,80 @@
+import 'package:dream_learn_app/screens/background.dart';
+import 'package:dream_learn_app/utils/text_field.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class ContactUS extends StatelessWidget {
+  const ContactUS({super.key});
+  
+  static final TextStyle _titleStyles = TextStyle(color:  Colors.black.withOpacity(0.5), fontSize: 34, letterSpacing: 5, fontWeight: FontWeight.bold);
+    static final TextStyle _contactDetailsStyles = TextStyle(color:  Colors.black, fontSize: 20, fontWeight: FontWeight.bold);
+
+    Widget _textContainer(Widget child){
+      return Padding(
+          padding: const EdgeInsets.only(left: 15),
+          child:child ,
+        );
+    }
+
+
+
+  Widget _passChild() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        const SizedBox(
+          height: 50,
+        ),
+        
+        const SizedBox(
+          height: 10,
+        ),
+         Container(
+          padding: EdgeInsets.only(left: 20),
+          decoration: BoxDecoration(
+            color: Color(0xffD9D9D9)
+          ),
+           child: Text(
+            'Contact Us',
+            style: _titleStyles,
+                   ),
+         ),
+      
+    
+        const SizedBox(
+          height: 100,
+        ),
+        _textContainer(
+        Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text('Telephone - ',style: _contactDetailsStyles,),
+              Column(
+                children: [
+                   Text('(+94) 2743473',style: _contactDetailsStyles,),
+                    Text('(8.00 pm - 6.00am )',style: _contactDetailsStyles,),
+                ],
+              )
+          
+            ],
+          ),
+        ),
+        const SizedBox(height: 35,),
+            _textContainer(
+        Text('Email - dreamlearnacademy@gmail.com',style: _contactDetailsStyles,),
+
+        ),
+
+         
+        
+
+      ],
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return BackgroundScreen(child: _passChild());
+  }
+}
