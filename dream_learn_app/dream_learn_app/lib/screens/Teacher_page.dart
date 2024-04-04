c878ab3ca3980241847eb6a29ee871c49432168b
Janod Abesekara <125848373+JanodAbesekara@users.noreply.github.com>
2024-04-04 11:19:15 +0530

    new edit the teacher dash bord

--- dream_learn_app/dream_learn_app/lib/screens/Teacher_page.dart
+++ dream_learn_app/dream_learn_app/lib/screens/Teacher_page.dart
@@ -4,8 +4,11 @@ import 'package:dream_learn_app/screens/background.dart';
 class TeacherDash extends StatefulWidget {
   const TeacherDash({Key? key}) : super(key: key);
 
+  @override
+  _TeacherDashState createState() => _TeacherDashState();
+}
 
-
+class _TeacherDashState extends State<TeacherDash> {
   @override
   Widget build(BuildContext context) {
     return BackgroundScreen(
@@ -15,7 +18,26 @@ class TeacherDash extends StatefulWidget {
 
   Widget _dashboard(BuildContext context) {
     return Center(
-    )
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          SizedBox(
+            height: 10,
+          ),
+          Image.asset(
+            "assets/teacherdash.png",
+            width: 400,
+            height: 300,
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          // Add your other widgets here
+          SizedBox(
+            height: 10,
+          ),
+        ],
+      ),
+    );
   }
-
 }
