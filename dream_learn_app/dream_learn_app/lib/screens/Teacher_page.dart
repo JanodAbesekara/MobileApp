7dccdb877e94631fb800231e9bb2807a60e59d12
Janod Abesekara <125848373+JanodAbesekara@users.noreply.github.com>
2024-04-09 06:41:45 +0530

    mobile app teacher dash bord

--- dream_learn_app/dream_learn_app/lib/screens/Teacher_page.dart
+++ dream_learn_app/dream_learn_app/lib/screens/Teacher_page.dart
@@ -9,6 +9,33 @@ class TeacherDash extends StatefulWidget {
 }
 
 class _TeacherDashState extends State<TeacherDash> {
+  final List<String> sampleImages = [
+    "assets/T/TAss.png",
+    "assets/T/Quises.png",
+    "assets/T/Tann.png",
+    "assets/T/Tattendence.png",
+    "assets/T/Tprofile.png",
+    "assets/T/TLerning.png",
+    "assets/T/TStudent.png",
+    "assets/T/Tgades.png",
+    "assets/T/Tfeed.png",
+  ];
+
+  final List<String> sampleNames = [
+    "TAss",
+    "Quises",
+    "Tann",
+    "Tattendence",
+    "Tprofile",
+    "TLerning",
+    "TStudent",
+    "Tgades",
+    "Tfeed",
+  ];
+
+  final double desiredWidth = 40.0; // Adjust according to your requirement
+  final double desiredHeight = 40.0; // Adjust according to your requirement
+
   @override
   Widget build(BuildContext context) {
     return BackgroundScreen(
@@ -16,44 +43,65 @@ class _TeacherDashState extends State<TeacherDash> {
     );
   }
 
- Widget _dashboard(BuildContext context) {
-  return Center(
-    child: Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        SizedBox(
-          height: 10,
-        ),
-        Image.asset(
-          "assets/teacherdash.png",
-          width: 400,
-          height: 300,
-        ),
-        SizedBox(
-          height: 10,
-        ),
-        Container(
-          padding: EdgeInsets.all(10),
-          decoration: BoxDecoration(
-            color: Color.fromARGB(255, 245, 241, 241),
-            borderRadius: BorderRadius.circular(10),
+  Widget _dashboard(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          SizedBox(
+            height: 10,
           ),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Icon(
-                Icons.person, 
-                color: Colors.blue,
-                size: 30,
-              ),
-            ],
+          Image.asset(
+            "assets/teacherdash.png",
+            width: 400,
+            height: 300,
           ),
-        ), // Add comma here
-        SizedBox(
-          height: 10,
-        ),
-      ],
-    ),
-  );
-}
+          SizedBox(
+            height: 10,
+          ),
+          GridView.builder(
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 3, // 3 columns
+              crossAxisSpacing: 2.0, // horizontal space between each item
+              mainAxisSpacing: 2.0, // vertical space between each item
+            ),
+            itemCount: sampleImages.length, // Number of sample images
+            shrinkWrap: true,
+            physics: NeverScrollableScrollPhysics(),
+            itemBuilder: (BuildContext context, int index) {
+              return Column(
+                children: [
+                  Container(
+                    padding: EdgeInsets.all(2),
+                    decoration: BoxDecoration(
+                      color: Color.fromARGB(255, 245, 241, 241),
+                      borderRadius: BorderRadius.circular(100),
+                    ),
+                    child: Image.asset(
+                      sampleImages[index],
+                      width: desiredWidth,
+                      height: desiredHeight,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                  SizedBox(height: 5), // Space between image and text
+                  Text(
+                    sampleNames[index], // Use sampleNames list to display image names
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 14,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
+              );
+            },
+          ),
+          SizedBox(
+            height: 10,
+          ),
+        ],
+      ),
+    );
+  }
 }
