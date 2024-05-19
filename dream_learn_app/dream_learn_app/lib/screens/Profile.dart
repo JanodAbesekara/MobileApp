d90466925a94c9b2b2e63efb9ea519f08122606a
Janod Abesekara <125848373+JanodAbesekara@users.noreply.github.com>
2024-05-19 11:20:24 +0530

    new edit

--- dream_learn_app/dream_learn_app/lib/screens/Profile.dart
+++ dream_learn_app/dream_learn_app/lib/screens/Profile.dart
@@ -18,7 +18,75 @@ class _ProfileState extends State<Profile> {
 
   Widget _profile(BuildContext context) {
     return Column(
-     
+      children: [
+        Container(
+          margin: EdgeInsets.only(top: 50.0), // Add margin to the top
+          width: 100.0, // Specify the width of the image
+          height: 100.0, // Specify the height of the image
+          decoration: BoxDecoration(
+            shape: BoxShape.circle, // Use BoxShape.circle for a circular border
+            border: Border.all(
+              color: Colors.blue, // Color of the border
+              width: 3.0, // Width of the border
+            ),
+          ),
+          child: ClipOval(
+            child: Image.asset(
+              'assets/profile_avatar.png', // Replace with your image URL or asset path
+              fit: BoxFit.cover,
+            ),
+          ),
+        ),
+        SizedBox(height: 10),
+        Text(
+          'John Doe', // Replace with the user's name
+          style: TextStyle(
+            fontSize: 20, // Font size
+            fontWeight: FontWeight.bold, // Font weight
+          ),
+        ),
+        Text(
+          '2024.2.12', // Replace with the user's date of birth
+          style: TextStyle(
+            fontSize: 13, // Font size
+            color: Colors.grey, // Text color
+          ),
+        ),
+        SizedBox(height: 20),
+        Container(
+          margin: EdgeInsets.symmetric(horizontal: 20), // Add margin to the container
+          decoration: BoxDecoration(
+            color: Colors.grey[300], // Background color of the container
+            borderRadius: BorderRadius.circular(20), // Border radius
+          ),
+          child: Padding(
+            padding: EdgeInsets.all(10), // Add padding to the container
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image.asset(
+                  'assets/Tprofile/tel.png', // Replace with your icon asset path
+                  width: 20,
+                  height: 20,
+                ),
+                SizedBox(width: 10), // Add spacing between icon and text
+                Image.asset(
+                  'assets/Tprofile/email.png', // Replace with your icon asset path
+                  width: 20,
+                  height: 20,
+                ),
+                SizedBox(width: 10), // Add spacing between icon and text
+                Text(
+                  ':-' + ' janod@gmail.com', // Replace with the user's email
+                  style: TextStyle(
+                    fontSize: 15, // Font size
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
