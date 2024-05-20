f2cec9faa2b5171f0e3bf69c181c8831c164fab0
Janod Abesekara <125848373+JanodAbesekara@users.noreply.github.com>
2024-05-20 10:50:52 +0530

    new edit

--- dream_learn_app/dream_learn_app/lib/screens/Profile.dart
+++ dream_learn_app/dream_learn_app/lib/screens/Profile.dart
@@ -11,82 +11,179 @@ class Profile extends StatefulWidget {
 class _ProfileState extends State<Profile> {
   @override
   Widget build(BuildContext context) {
-    return BackgroundScreen(
-      child: _profile(context),
+    return Scaffold(
+      body: Stack(
+        children: [
+          GestureDetector(
+            onTapUp: (details) {
+              if (details.localPosition.dx < 50 && details.localPosition.dy < 50) {
+                Navigator.of(context).pop();
+              }
+            },
+            child: BackgroundScreen(
+              child: _profile(context),
+            ),
+          ),
+          Positioned(
+            top: 0,
+            left: 0,
+            child: SizedBox(
+              width: 80,
+              height: 80,
+              child: GestureDetector(
+                onTap: () => Navigator.of(context).pop(),
+                child: Container(
+                  color: Colors.transparent, // Invisible area
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 
   Widget _profile(BuildContext context) {
-    return Column(
-      children: [
-        Container(
-          margin: EdgeInsets.only(top: 50.0), // Add margin to the top
-          width: 100.0, // Specify the width of the image
-          height: 100.0, // Specify the height of the image
-          decoration: BoxDecoration(
-            shape: BoxShape.circle, // Use BoxShape.circle for a circular border
-            border: Border.all(
-              color: Colors.blue, // Color of the border
-              width: 3.0, // Width of the border
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          Container(
+            margin: EdgeInsets.only(top: 50.0), // Margin from top
+            width: 100.0, // Width of the image container
+            height: 100.0, // Height of the image container
+            decoration: BoxDecoration(
+              shape: BoxShape.circle, // Circular shape
+              border: Border.all(
+                color: Colors.blue, // Border color
+                width: 3.0, // Border width
+              ),
+            ),
+            child: ClipOval(
+              child: Image.asset(
+                'assets/profile_avatar.png', // Image asset path
+                fit: BoxFit.cover,
+              ),
             ),
           ),
-          child: ClipOval(
-            child: Image.asset(
-              'assets/profile_avatar.png', // Replace with your image URL or asset path
-              fit: BoxFit.cover,
+          SizedBox(height: 10),
+          // User Name Text
+          Text(
+            'John Doe', // User's name
+            style: TextStyle(
+              fontSize: 20, // Font size
+              fontWeight: FontWeight.bold, // Bold font weight
             ),
           ),
-        ),
-        SizedBox(height: 10),
-        Text(
-          'John Doe', // Replace with the user's name
-          style: TextStyle(
-            fontSize: 20, // Font size
-            fontWeight: FontWeight.bold, // Font weight
+          // User's Date of Birth Text
+          Text(
+            '2024.2.12', // User's date of birth
+            style: TextStyle(
+              fontSize: 13, // Font size
+              color: Colors.grey, // Text color
+            ),
           ),
-        ),
-        Text(
-          '2024.2.12', // Replace with the user's date of birth
-          style: TextStyle(
-            fontSize: 13, // Font size
-            color: Colors.grey, // Text color
-          ),
-        ),
-        SizedBox(height: 20),
-        Container(
-          margin: EdgeInsets.symmetric(horizontal: 20), // Add margin to the container
-          decoration: BoxDecoration(
-            color: Colors.grey[300], // Background color of the container
-            borderRadius: BorderRadius.circular(20), // Border radius
-          ),
-          child: Padding(
-            padding: EdgeInsets.all(10), // Add padding to the container
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Image.asset(
-                  'assets/Tprofile/tel.png', // Replace with your icon asset path
-                  width: 20,
-                  height: 20,
-                ),
-                SizedBox(width: 10), // Add spacing between icon and text
-                Image.asset(
-                  'assets/Tprofile/email.png', // Replace with your icon asset path
-                  width: 20,
-                  height: 20,
-                ),
-                SizedBox(width: 10), // Add spacing between icon and text
-                Text(
-                  ':-' + ' janod@gmail.com', // Replace with the user's email
-                  style: TextStyle(
-                    fontSize: 15, // Font size
+          SizedBox(height: 20),
+          // Contact Information Container
+          Container(
+            margin: EdgeInsets.symmetric(horizontal: 50), // Horizontal margin
+            decoration: BoxDecoration(
+              color: Colors.grey[300], // Background color
+              borderRadius: BorderRadius.circular(20), // Rounded corners
+            ),
+            child: Padding(
+              padding: EdgeInsets.all(30.0), // Padding inside the container
+              child: Column(
+                children: [
+                  // Phone Number Row
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.start, // Start alignment
+                    children: [
+                      Image.asset(
+                        'assets/Tprofile/tel.png', // Phone icon asset path
+                        width: 20,
+                        height: 20,
+                      ),
+                      SizedBox(width: 10),
+                      Expanded(
+                        child: Text(
+                          ':- 012312321', // User's phone number
+                          style: TextStyle(
+                            fontSize: 15, // Font size
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
-                ),
-              ],
+                  SizedBox(height: 10),
+                  // Email Row
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.start, // Start alignment
+                    children: [
+                      Image.asset(
+                        'assets/Tprofile/email.png', // Email icon asset path
+                        width: 20,
+                        height: 20,
+                      ),
+                      SizedBox(width: 10),
+                      Expanded(
+                        child: Text(
+                          ':- janod@gmail.com', // User's email
+                          style: TextStyle(
+                            fontSize: 15, // Font size
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 10),
+                  // Location Row
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.start, // Start alignment
+                    children: [
+                      Image.asset(
+                        'assets/Tprofile/location.png', // Location icon asset path
+                        width: 20,
+                        height: 20,
+                      ),
+                      SizedBox(width: 10),
+                      Expanded(
+                        child: Text(
+                          ':- Penalikadawara, Moragene', // User's location
+                          style: TextStyle(
+                            fontSize: 15, // Font size
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 10),
+                  // Subjects Row
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.start, // Start alignment
+                    children: [
+                      Image.asset(
+                        'assets/Tprofile/classes.png', // Subjects icon asset path
+                        width: 20,
+                        height: 20,
+                      ),
+                      SizedBox(width: 10),
+                      Expanded(
+                        child: Text(
+                          ':- Chemistry,  Physics,  Biology', // User's subjects
+                          style: TextStyle(
+                            fontSize: 15, // Font size
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
             ),
           ),
-        ),
-      ],
+          SizedBox(height: 100),
+        ],
+      ),
     );
   }
 }
--- dream_learn_app/dream_learn_app/lib/screens/QuizessT.dart
+++ dream_learn_app/dream_learn_app/lib/screens/QuizessT.dart
@@ -1,19 +1,73 @@
 import 'package:flutter/material.dart';
 import 'package:dream_learn_app/screens/background.dart';
 
-class  QuizessT extends StatelessWidget {
-  const  QuizessT({Key? key}) : super(key: key);
+class QuizessT extends StatefulWidget {
+  const QuizessT({Key? key}) : super(key: key);
 
   @override
+  _QuizessTState createState() => _QuizessTState();
+}
+
+class _QuizessTState extends State<QuizessT> {
+  @override
   Widget build(BuildContext context) {
-    return BackgroundScreen(
-      child: _passChild(context),
+    return Scaffold(
+      body: Stack(
+        children: [
+          GestureDetector(
+            onTapUp: (details) {
+              if (details.localPosition.dx < 50 &&
+                  details.localPosition.dy < 50) {
+                Navigator.of(context).pop();
+              }
+            },
+            child: BackgroundScreen(
+              child: _buildChild(context),
+            ),
+          ),
+          Positioned(
+            top: 0,
+            left: 0,
+            child: SizedBox(
+              width: 80,
+              height: 80,
+              child: GestureDetector(
+                onTap: () => Navigator.of(context).pop(),
+                child: Container(
+                  color: Colors.transparent, // Invisible area
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 
-  Widget _passChild(BuildContext context) {
-    return Column(
-      
+  Widget _buildChild(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(top: 50.0, bottom: 50.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            'Quizzes',
+            style: TextStyle(
+              fontSize: 30.0,
+              fontWeight: FontWeight.bold,
+              fontFamily: 'Montserrat',
+              color: Colors.blue,
+            ),
+          ),
+          SizedBox(height: 20.0), // Add spacing between the title and row
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
