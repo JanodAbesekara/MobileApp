44c9b57bd211f249452878fb6cd70f9183a3ccec
Janod Abesekara <125848373+JanodAbesekara@users.noreply.github.com>
2024-05-27 21:52:23 +0530

    new edit

--- dream_learn_app/dream_learn_app/lib/screens/Profile.dart
+++ dream_learn_app/dream_learn_app/lib/screens/Profile.dart
@@ -1,24 +1,129 @@
 import 'package:flutter/material.dart';
-import 'package:dream_learn_app/screens/background.dart';
+import 'package:dream_learn_app/screens/AppInsideBackground.dart';
 
-class Profile extends StatefulWidget {
+class Profile extends StatelessWidget {
   const Profile({Key? key}) : super(key: key);
 
   @override
-  _ProfileState createState() => _ProfileState();
-}
-
-class _ProfileState extends State<Profile> {
-  @override
   Widget build(BuildContext context) {
-    return BackgroundScreen(
-      child: _profile(context),
+    return Scaffold(
+      body: Stack(
+        children: [
+          BackgroundScreen(
+            child: _buildProfile(),
+          ),
+          Positioned(
+            top: 40,
+            left: 20,
+            child: IconButton(
+              icon: Icon(Icons.arrow_back_ios),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ),
+        ],
+      ),
     );
   }
 
-  Widget _profile(BuildContext context) {
+  Widget _buildProfile() {
     return Column(
-     
+      children: [
+        _buildAvatar(),
+        _buildNameAndDate(),
+        _buildContactInfo(),
+      ],
+    );
+  }
+
+  Widget _buildAvatar() {
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(60.0, 20.0, 60.0, 20.0),
+      child: Image.asset(
+        'assets/profile_avatar.png',
+        width: 191,
+        height: 189,
+      ),
+    );
+  }
+
+  Widget _buildNameAndDate() {
+    return Column(
+      children: [
+        Text(
+          'John Doe', // Replace with actual name
+          style: TextStyle(
+            color: Color(0xFF180565),
+            fontSize: 20,
+            letterSpacing: 2,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        Text(
+          '2010.09.12', // Replace with actual date
+          style: TextStyle(
+            color: Color.fromARGB(255, 152, 6, 231),
+            fontSize: 12,
+            letterSpacing: 1.0,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        SizedBox(height: 40), // Added SizedBox here
+      ],
+    );
+  }
+
+  Widget _buildContactInfo() {
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(40.0, 20.0, 30.0, 20.0),
+      child: Column(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              border: Border.all(
+                color: Color.fromARGB(255, 223, 221, 231),
+              ),
+              borderRadius: BorderRadius.circular(15),
+              color: Color.fromARGB(
+                  131, 139, 137, 149), // Add background color to the container
+            ),
+            child: Column(
+              children: [
+                _buildContactRow(Icons.email, ':-  Twasda@gmail.com'),
+                _buildContactRow(Icons.phone, ':-  0123456789'),
+                _buildContactRow(Icons.location_on, ':-  Phnom Penh, Cambodia'),
+                _buildContactRow(Icons.class_,
+                    ':-  Math, Science, English'), // changed from 'classes' to 'class_'
+              ],
+            ),
+          ),
+          SizedBox(height: 60), // Added SizedBox here
+        ],
+      ),
+    );
+  }
+
+  Widget _buildContactRow(IconData icon, String text) {
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(
+          30.0, 10.0, 40.0, 20.0), // Removed bottom padding here
+      child: Row(
+        children: [
+          Icon(icon),
+          SizedBox(width: 10),
+          Text(
+            text,
+            style: TextStyle(
+              color: Color.fromARGB(255, 61, 60,
+                  73), // Change text color to contrast with the container background color
+              fontSize: 15,
+              letterSpacing: 1.0,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
--- dream_learn_app/dream_learn_app/lib/screens/StudentT.dart
+++ dream_learn_app/dream_learn_app/lib/screens/StudentT.dart
@@ -4,21 +4,154 @@ import 'package:dream_learn_app/screens/background.dart';
 class StudentT extends StatelessWidget {
   const StudentT({Key? key}) : super(key: key);
 
-  static const _loginTextStyles = TextStyle(
-    color: Color(0xff4E919A),
-    fontSize: 20,
-    letterSpacing: 10,
-    fontWeight: FontWeight.bold,
-  );
-
   @override
   Widget build(BuildContext context) {
-    return BackgroundScreen(
-      child: _passChild(context),
+    return Scaffold(
+      body: Stack(
+        children: [
+          BackgroundScreen(
+            child: _passChild(context),
+          ),
+          Positioned(
+            top: 50,
+            left: 20,
+            child: IconButton(
+              icon: Icon(Icons.arrow_back_ios),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ),
+        ],
+      ),
     );
   }
 
   Widget _passChild(BuildContext context) {
-    return Column();
+    return Column(
+      
+      children: [
+        StudentCard(
+          name: 'John Doe',
+          mediem: 'Shinhala',
+          subject: 'Mathematics',
+          parentsnamel: 'jagath nimal',
+          parentspho: '0712345678',
+          parentsEmail: 'dasdfafad@zfd',
+
+        ),
+        StudentCard(
+          name: 'Jane Doe',
+          mediem: 'English',
+          subject: 'English',
+          parentsnamel: 'samn kumara',
+          parentspho: '0712345678',
+          parentsEmail: 'dasdfafad@zfd',
+        ),
+      ],
+    );
+  }
+}
+
+class StudentCard extends StatelessWidget {
+  final String name;
+  final String mediem;
+  final String subject;
+  final String parentsnamel;
+  final String parentspho;
+  final String parentsEmail;
+
+  const StudentCard({
+    Key? key,
+    required this.name,
+    required this.mediem,
+    required this.subject,
+    required this.parentsnamel,
+    required this.parentspho,
+    required this.parentsEmail,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(20.0),
+      child: Container(
+        width: double.infinity,
+        height: 200,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(10),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey.withOpacity(0.5),
+              spreadRadius: 5,
+              blurRadius: 7,
+              offset: Offset(0, 3),
+            ),
+          ],
+        ),
+        child: Row(
+          children: [
+            Padding(
+              padding: const EdgeInsets.fromLTRB(20.0, 20.0, 40.0, 10.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    name,
+                    style: TextStyle(
+                      color: Color(0xFF180565),
+                      fontSize: 20,
+                      letterSpacing: 2,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Text(
+                    subject,
+                    style: TextStyle(
+                      color: Color(0xFF180565),
+                      fontSize: 16,
+                      letterSpacing: 2,
+                    ),
+                  ),
+                  Text(
+                    mediem,
+                    style: TextStyle(
+                      color: Color(0xFF180565),
+                      fontSize: 16,
+                      letterSpacing: 2,
+                    ),
+                  ),
+                  Text(
+                    parentsnamel,
+                    style: TextStyle(
+                      color: Color(0xFF180565),
+                      fontSize: 16,
+                      letterSpacing: 2,
+                    ),
+                  ),
+                  Text(
+                    parentspho,
+                    style: TextStyle(
+                      color: Color(0xFF180565),
+                      fontSize: 16,
+                      letterSpacing: 2,
+                    ),
+                  ),
+                  Text(
+                    parentsEmail,
+                    style: TextStyle(
+                      color: Color(0xFF180565),
+                      fontSize: 16,
+                      letterSpacing: 2,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
