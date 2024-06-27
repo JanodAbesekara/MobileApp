225ddf4522dbd294f5fd6b750a850399c0359a67
Janod Abesekara <125848373+JanodAbesekara@users.noreply.github.com>
2024-06-27 13:23:42 +0530

    edit

--- dream_learn_app/dream_learn_app/lib/screens/StudentT.dart
+++ dream_learn_app/dream_learn_app/lib/screens/StudentT.dart
@@ -12,20 +12,7 @@ class StudentT extends StatelessWidget {
       body: Stack(
         children: [
           BackgroundScreen(
-            child: FutureBuilder<List<Map<String, dynamic>>?>(
-              future: StudentparentsService.getStudentList(),
-              builder: (context, snapshot) {
-                if (snapshot.connectionState == ConnectionState.waiting) {
-                  return Center(child: CircularProgressIndicator());
-                } else if (snapshot.hasError) {
-                  return Center(child: Text("Error: ${snapshot.error}"));
-                } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
-                  return Center(child: Text("No students found"));
-                } else {
-                  return _buildStudentList(snapshot.data!);
-                }
-              },
-            ),
+            child: _buildFutureBuilder(context),
           ),
           Positioned(
             top: 50,
@@ -42,32 +29,45 @@ class StudentT extends StatelessWidget {
     );
   }
 
+  Widget _buildFutureBuilder(BuildContext context) {
+    return FutureBuilder<List<Map<String, dynamic>>?>(
+      future: StudentparentsService.getStudentList(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Center(child: CircularProgressIndicator());
+        } else if (snapshot.hasError) {
+          return Center(child: Text('Error: ${snapshot.error}'));
+        } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+          return Center(child: Text('No students found'));
+        } else {
+          return _buildStudentList(snapshot.data!);
+        }
+      },
+    );
+  }
+
   Widget _buildStudentList(List<Map<String, dynamic>> students) {
-    return Expanded(
-      child: ListView.builder(
-        itemCount: students.length,
-        itemBuilder: (context, index) {
-          final student = students[index];
-          final profile = student['profile'] ?? {};
-          return StudentCard(
-            name: profile['name'] ?? 'N/A',
-            medium: student['Enmedium'] ?? 'N/A',
-            subject: student['Ensubject'] ?? 'N/A',
-            studentEmail: student['userEmail'] ?? 'N/A',
-            parentsName: profile['name'] ?? 'N/A',
-            parentsPhone: profile['mobileNo']?.toString() ?? 'N/A',
-            parentsEmail: profile['email'] ?? 'N/A',
-            profile: profile['url'] ?? 'assets/profile_avatar.png',
-          );
-        },
-      ),
+    return ListView.builder(
+      itemCount: students.length,
+      itemBuilder: (context, index) {
+        final student = students[index];
+        final profile = student['profile'] ?? {};
+        return StudentCard(
+          profile: profile['url'] ?? 'assets/profile_avatar.png',
+          subject: student['Ensubject'] ?? 'N/A',
+          studentEmail: student['userEmail'] ?? 'N/A',
+          medium: student['Enmedium'] ?? 'N/A',
+          parentsName: profile['name'] ?? 'N/A',
+          parentsPhone: profile['mobileNo']?.toString() ?? 'N/A',
+          parentsEmail: profile['email'] ?? 'N/A',
+        );
+      },
     );
   }
 }
 
 class StudentCard extends StatelessWidget {
   final String profile;
-  final String name;
   final String subject;
   final String studentEmail;
   final String medium;
@@ -77,7 +77,6 @@ class StudentCard extends StatelessWidget {
 
   StudentCard({
     required this.profile,
-    required this.name,
     required this.subject,
     required this.studentEmail,
     required this.medium,
@@ -91,7 +90,6 @@ class StudentCard extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.all(20.0),
       child: Container(
-        width: double.infinity,
         decoration: BoxDecoration(
           color: Colors.white,
           borderRadius: BorderRadius.circular(10),
@@ -109,39 +107,28 @@ class StudentCard extends StatelessWidget {
           child: Row(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    name,
-                    style: TextStyle(
-                      color: Color(0xFF180565),
-                      fontSize: 20,
-                      letterSpacing: 2,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  _buildInfoText('StudentEmail', studentEmail),
-                  _buildInfoText('Subject', subject),
-                  _buildInfoText('Medium', medium),
-                  _buildInfoText('ParentsName', parentsName),
-                  _buildInfoText('ParentsPhone', parentsPhone),
-                  _buildInfoText('ParentsEmail', parentsEmail),
-                ],
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    _buildInfoText('Student Email', studentEmail),
+                    _buildInfoText('Subject', subject),
+                    _buildInfoText('Medium', medium),
+                    _buildInfoText('Parent\'s Name', parentsName),
+                    _buildInfoText('Parent\'s Phone', parentsPhone),
+                    _buildInfoText('Parent\'s Email', parentsEmail),
+                  ],
+                ),
               ),
               SizedBox(width: 20),
-              Expanded(
-                child: Align(
-                  alignment: Alignment.topRight,
-                  child: Image.network(
-                    profile,
-                    width: 80,
-                    height: 80,
-                    errorBuilder: (context, error, stackTrace) {
-                      return Image.asset('assets/profile_avatar.png', width: 80, height: 80);
-                    },
-                  ),
-                ),
+              Image.network(
+                profile,
+                width: 80,
+                height: 80,
+                errorBuilder: (context, error, stackTrace) {
+                  return Image.asset('assets/profile_avatar.png',
+                      width: 80, height: 80);
+                },
               ),
             ],
           ),
@@ -163,12 +150,14 @@ class StudentCard extends StatelessWidget {
               color: Color(0xFF222831),
             ),
           ),
-          Text(
-            value,
-            style: TextStyle(
-              color: Color(0xFF180565),
-              fontSize: 13,
-              letterSpacing: 2,
+          Expanded(
+            child: Text(
+              value,
+              style: TextStyle(
+                color: Color(0xFF180565),
+                fontSize: 13,
+                letterSpacing: 2,
+              ),
             ),
           ),
         ],
--- dream_learn_app/dream_learn_app/lib/services/Studentparents_service.dart
+++ dream_learn_app/dream_learn_app/lib/services/Studentparents_service.dart
@@ -3,7 +3,7 @@ import 'package:shared_preferences/shared_preferences.dart';
 import 'package:jwt_decode/jwt_decode.dart';
 import 'dart:convert';
 
-class StudentparentsService{
+class StudentparentsService {
   static Future<List<Map<String, dynamic>>?> getStudentList() async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
     String? token = prefs.getString('auth_token');
@@ -12,31 +12,45 @@ class StudentparentsService{
       throw Exception('Token not found');
     }
 
-    Map<String, dynamic> decodedToken = Jwt.parseJwt(token);
-    String? email = decodedToken['email'];
+    Map<String, dynamic> decodedToken;
+    try {
+      decodedToken = Jwt.parseJwt(token);
+    } catch (e) {
+      throw Exception('Invalid token format: $e');
+    }
 
+    String? email = decodedToken['email'];
     if (email == null) {
       throw Exception('Email not found in token');
     }
 
-    var url = Uri.http('bytegroupproject.onrender.com', '/api/assignment/eachsubstudents', {'email': email});
-    var response = await http.get(url);
+    var url = Uri.http('bytegroupproject.onrender.com',
+        '/api/assignment/eachsubstudents', {'email': email});
+    var response;
+    try {
+      response = await http.get(url);
+    } catch (e) {
+      throw Exception('Failed to send request: $e');
+    }
 
     if (response.statusCode == 200) {
+      var responseBody;
       try {
-        var responseBody = jsonDecode(response.body);
-        if (responseBody.containsKey('data')) {
-          List<dynamic> studentList = responseBody['data'];
-          print(studentList);
-          return studentList.cast<Map<String, dynamic>>();
-        } else {
-          throw Exception('Data field not found in response');
-        }
+        responseBody = jsonDecode(response.body);
       } catch (e) {
         throw Exception('Error decoding JSON: $e');
       }
+
+      if (responseBody is Map<String, dynamic> &&
+          responseBody.containsKey('data')) {
+        List<dynamic> studentList = responseBody['data'];
+        return studentList.cast<Map<String, dynamic>>();
+      } else {
+        throw Exception('Data field not found in response');
+      }
     } else {
-      throw Exception('Cannot get student list');
+      throw Exception(
+          'Cannot get student list, status code: ${response.statusCode}');
     }
   }
 }
