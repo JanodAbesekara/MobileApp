231c24b2f537751380851db2a27cd1bce7f9c353
Janod Abesekara <125848373+JanodAbesekara@users.noreply.github.com>
2024-06-27 20:16:51 +0530

    fix the profile

--- dream_learn_app/dream_learn_app/lib/services/ProfileService.dart
+++ dream_learn_app/dream_learn_app/lib/services/ProfileService.dart
@@ -1,11 +1,11 @@
-import 'package:http/http.dart' as http;
+// File: ProfileService.dart
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:jwt_decode/jwt_decode.dart';
+import 'package:http/http.dart' as http;
 import 'dart:convert';
 
 class ProfileService {
-  // Method to decode the token and get the email
-  static Future<String?> getEmailFromToken() async {
+  static Future<List<Map<String, dynamic>>?> getProfileDataFromToken() async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
     String? token = prefs.getString('auth_token');
     if (token == null) {
@@ -15,17 +15,16 @@ class ProfileService {
     String? email = decodedToken['email'];
 
     if (email != null) {
-      var url = Uri.http('bytegroupproject.onrender.com',
-          '/api/Test/getlecturefulldtails', {'email': email});
+      var url = Uri.parse(
+          'https://bytegroupproject.onrender.com/api/Test/getlecturefulldtails?email=$email');
       var response = await http.get(url);
 
       if (response.statusCode == 200) {
         try {
           var responseBody = jsonDecode(response.body);
           if (responseBody.containsKey('data')) {
-            List<dynamic> profileData = responseBody['data'];
-            print(profileData);
-            return profileData.cast<Map<String, dynamic>>().first['email'];
+            List<dynamic> profileList = responseBody['data'];
+            return profileList.cast<Map<String, dynamic>>();
           } else {
             throw Exception('Data field not found in response');
           }
@@ -37,5 +36,22 @@ class ProfileService {
       }
     }
     return null;
+  }
+
+  static Future<Map<String, String>?> getNameFromToken() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    String? token = prefs.getString('auth_token');
+    if (token == null) {
+      throw Exception('Token not found');
+    }
+    Map<String, dynamic> decodedToken = Jwt.parseJwt(token);
+    String? firstName = decodedToken['firstname'];
+    String? lastName = decodedToken['lastname'];
+
+    if (firstName != null && lastName != null) {
+      return {'firstName': firstName, 'lastName': lastName};
+    } else {
+      throw Exception('First name or last name not found in token');
+    }
   }
 }
