0cec7bba43774dbe8b3c4ebbab971168a5c4a1af
Janod Abesekara <125848373+JanodAbesekara@users.noreply.github.com>
2024-07-06 09:40:24 +0530

    Update lecture_material_service.dart

--- dream_learn_app/dream_learn_app/lib/services/lecture_material_service.dart
+++ dream_learn_app/dream_learn_app/lib/services/lecture_material_service.dart
@@ -2,25 +2,26 @@ import 'package:dream_learn_app/models/lecture_material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
-class LectureMaterialService{
-  static Future<List<LectureMaterial>> getLectureMaterials(String teacherEmail,String subject,String medium)async{
-  var url = Uri.http('10.0.2.2:6000', '/api/Quise/getlecturematerial');
-  var response =await http.post(url, body: {'teacheremail':teacherEmail , 'subject':subject,'medium':medium });
-   Map<String, dynamic> decodedJson= jsonDecode(response.body)  as Map<String, dynamic>;
-   final lecMaterialList= decodedJson['data'] as List;
+class LectureMaterialService {
+  static Future<List<LectureMaterial>> getLectureMaterials(
+      String teacherEmail, String subject, String medium) async {
+    var url = Uri.http('bytegroupproject.onrender.com', '/api/Test/getlecturematerial',
+        {'teachermail': teacherEmail, 'subject': subject, 'medium': medium});
+    var response = await http.get(
+      url,
+    );
+    Map<String, dynamic> decodedJson =
+        jsonDecode(response.body) as Map<String, dynamic>;
+    final lecMaterialList = decodedJson['data'] as List;
 
-  if(response.statusCode==200){
-  return lecMaterialList.map((item) => LectureMaterial.fromJson(item)).toList();
-   
-  
-  }else{
-    throw 'cannot load lecture materials';
+    if (response.statusCode == 200) {
+      return lecMaterialList
+          .map((item) => LectureMaterial.fromJson(item))
+          .toList();
+
+      print(lecMaterialList);
+    } else {
+      throw 'cannot load lecture materials';
+    }
   }
-
- 
-
-
-
-
- }
 }
